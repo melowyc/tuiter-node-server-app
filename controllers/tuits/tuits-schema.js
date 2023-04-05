31c726dda91b63324110fdb4a57c88355a1c59b0
Yichao Wu <yichaowu0316@gmail.com>
2023-04-04 18:15:09 -0700

    a9 mongo sp23 debug1

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,5 +3,16 @@ const schema = mongoose.Schema({
     tuit: String,
     likes: Number,
     liked: Boolean,
+
+    topic:String,
+    userName:String,
+    handle:String,
+    time:String,
+    image:String,
+    title:String,
+    replies:Number,
+    retuits:Number,
+    disliked:Boolean,
+    dislikes:Number,
 }, {collection: 'tuits'});
 export default schema;
