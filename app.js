0bd555a8b3a6e0f78c40d3324278f32237f193da
Yichao Wu <yichaowu0316@gmail.com>
2023-04-04 20:41:47 -0700

    a9 mongo sp23 debug2

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
+console.log(CONNECTION_STRING);
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
